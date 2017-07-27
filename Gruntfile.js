b652d5f92ee408fb51ee47a76df1e71a3a6b7427
Hack Reactor Student <student@hackreactor.com>
2017-07-26 20:00:31 -0500

    added grunt concat functionality

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,13 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['public/client/**/*.js', 'public/lib/**/*.js'],
+        dest: 'public/dist/clientmin.js',
+      },
     },
 
     mochaTest: {
@@ -21,6 +28,10 @@ module.exports = function(grunt) {
     },
 
     uglify: {
+      dist: {
+        src: ['public/client/**/*.js', 'public/lib/**/*.js'],
+        dest: 'dist/basic.js',
+      },
     },
 
     eslint: {
